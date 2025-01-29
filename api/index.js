4d4682da3d6bca9a1a6e2a21d67a8ba0fa619e16
arsalalqital <arsalalqital@gmail.com>
2025-01-30 01:19:18 +0700

    pesan pertama

--- api/index.js
+++ api/index.js
@@ -1,24 +1,13 @@
-
 const express = require('express');
 const app = express();
-const port = 3000;
-
 
 app.get('/', (req, res) => {
-  res.send('Selamat datang di halaman utama!');
+    res.send('Hello from Express.js on Vercel!');
+});
+
+app.get('/api/data', (req, res) => {
+    res.json({ message: 'Ini data JSON dari Express di Vercel' });
 });
 
 
-app.get('/tentang', (req, res) => {
-  res.send('Ini adalah halaman tentang kami.');
-});
-
-
-app.get('/kontak', (req, res) => {
-  res.send('Hubungi kami di kontak@example.com.');
-});
-
-
-app.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
-});
+module.exports = app;
